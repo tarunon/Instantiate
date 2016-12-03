c375fd7c17d47d9caaa4987f167a5faea99d3fbd
齋藤暢郎 <tarunon@saitoutoorurounomacbook-pro.local>
2016-12-03 16:00:53 +0900

    Initial Commit

---
+++ Instantiate/Instantiate.h
@@ -0,0 +1,19 @@
+//
+//  Instantiate.h
+//  Instantiate
+//
+//  Created by 齋藤暢郎 on 2016/12/03.
+//  Copyright © 2016年 tarunon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Instantiate.
+FOUNDATION_EXPORT double InstantiateVersionNumber;
+
+//! Project version string for Instantiate.
+FOUNDATION_EXPORT const unsigned char InstantiateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Instantiate/PublicHeader.h>
+
+
---
+++ InstantiateTests/InstantiateTests.swift
@@ -0,0 +1,36 @@
+//
+//  InstantiateTests.swift
+//  InstantiateTests
+//
+//  Created by 齋藤暢郎 on 2016/12/03.
+//  Copyright © 2016年 tarunon. All rights reserved.
+//
+
+import XCTest
+@testable import Instantiate
+
+class InstantiateTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
