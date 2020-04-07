0a4123e0460c2e8d9c5d08e82df17c584ceded97
mtfum <f.yamanaka1127@gmail.com>
2020-04-07 20:37:47 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,22 +1,38 @@
-// swift-tools-version:3.1
+// swift-tools-version:5.2
 
 import PackageDescription
-import Foundation
-
-let libralies = [
-    Target(
-        name: "Instantiate"
-    ),
-    Target(
-        name: "InstantiateStandard",
-        dependencies: [
-            .Target(name: "Instantiate")
-        ]
-    )
-]
 
 let package = Package(
     name: "Instantiate",
-    targets: libralies,
-    swiftLanguageVersions: [3, 4]
+    platforms: [
+        .iOS(.v9),
+        .tvOS(.v9),
+        .macOS(.v10_11)
+    ],
+    products: [
+        .library(
+            name: "Instantiate",
+            targets: ["Instantiate"]
+        ),
+        .library(
+            name: "InstantiateStandard",
+            targets: ["InstantiateStandard"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "Instantiate"
+        ),
+        .target(
+            name: "InstantiateStandard",
+            dependencies: [
+                .target(name: "Instantiate")
+            ]
+        ),
+        .testTarget(
+            name: "InstantiateTests",
+            dependencies: ["Instantiate"]
+        )
+    ],
+    swiftLanguageVersions: [.v4, .v5]
 )
